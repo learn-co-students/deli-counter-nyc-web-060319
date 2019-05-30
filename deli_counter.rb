424172c09d67057383643a14f01445abfb616d81
Cindy Chen <chen_cx0128@hotmail.com>
2019-05-30 19:59:06 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+katz_deli = [ ]
+
+def line(katz_deli)
+  i = 0
+  while i < katz_deli.size
+  "The line is currently: #{i+1}. katz_deli[i]"
+  i++
+end
+
+puts "The line is currently empty."
